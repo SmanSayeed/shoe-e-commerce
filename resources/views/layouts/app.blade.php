023eefcc54b92d1e608fe660e7ba2f3fe5892257
000kawser@gmail.com <kk?2459786123>
2025-11-12 23:22:58 +0600

    -site settings added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }}</title>
+    @php
+        $faviconUrl = \App\Helpers\SiteSettingsHelper::faviconUrl();
+    @endphp
+    @if($faviconUrl)
+        <link rel="icon" href="{{ $faviconUrl }}" type="image/x-icon">
+    @endif
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/user/app.css', 'resources/js/user/app.js'])
     @stack('styles')
